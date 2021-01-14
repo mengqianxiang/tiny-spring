9fb85d5c10461248472ba37d1859061ad9460fc2
mengqianxiang <qx.meng@zuolin.com>
2021-01-14 14:57:03 +0800

    项目报错修改。

--- src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
+++ src/test/java/us/codecraft/tinyioc/OutputServiceImpl.java
@@ -5,6 +5,16 @@ package us.codecraft.tinyioc;
  */
 public class OutputServiceImpl implements OutputService {
 
+    private HelloWorldService helloWorldService;
+
+    public HelloWorldService getHelloWorldService() {
+        return helloWorldService;
+    }
+
+    public void setHelloWorldService(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
     @Override
     public void output(String text){
         System.out.println(text);
